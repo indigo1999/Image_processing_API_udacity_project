4f00fea0f270a30fa489ebf1265b2c47e0819c0e
Komgrich Srisak <komgrichsrisak@MacBook-Pro-khxng-Komgrich.local>
2025-08-22 22:59:53 +0700

    first step...

--- build/index.js
+++ build/index.js
@@ -1,7 +1,14 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const port = 3000; //process.env.DEV_PORT_EXERCISE
 const hello = (name) => {
     console.log(`Glad to hear your news ${name} .`);
+    console.log(port);
 };
 hello("Game");
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,26 @@
 
+import express, { request, response } from 'express';
+
+const app = express();
+const port = 3000//process.env.DEV_PORT_EXERCISE
+
+///Listen On Port
+app.listen(port, () => {
+    console.log(`Server started at 127.0.0.1:${port}`)
+})
+
+
+
+///GET
+app.get('/api', (request , response) => {
+    response.send("Hello , World!");
+})
+
+
+
 const hello = (name : string) : void => {
     console.log(`Glad to hear your news ${name} .`)
+    console.log(port)
 }
 
 hello("Game")
