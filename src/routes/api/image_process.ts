327373f646069e28b85de6e8842230a66bc72833
Komgrich Srisak <komgrichsrisak@MacBook-Pro-khxng-Komgrich.local>
2025-08-24 14:59:59 +0700

    write more conditions for filename inexistence

--- src/routes/api/image_process.ts
+++ src/routes/api/image_process.ts
@@ -1,4 +1,5 @@
 import { error } from 'console';
+import e from 'express';
 import express, { Response } from 'express';
 import { promises as fs_promises } from 'fs'
 import path from 'path'
@@ -10,40 +11,42 @@ const image_routes = express.Router();
 image_routes.get('/',async (request,response) => {
 
     let filename : string = ""//request.query.filename?.toString()!
+    let width_query : number = parseInt(request.query.width?.toString()!,10)
+    let height_query : number = parseInt(request.query.height?.toString()!,10)
+
 
     if(request.query.filename && request.query.filename?.toString()!.length != 0) {
         filename = request.query.filename?.toString()!
     } else {
         response.status(400).send("No filename input")
     }
-    let width_query : number = parseInt(request.query.width?.toString()!,10)
-    let height_query : number = parseInt(request.query.height?.toString()!,10)
-
-
-    try {
-        if ( (isNaN(width_query) || width_query <= 0) && (isNaN(height_query) || height_query <= 0) ) {
-            response.status(400).send('Invalid width and height parameter.');
-        } else {
-           resize_image(filename,width_query,height_query,response)
-        }
-    } catch (error) {
-        response.status(400).send('Invalid width parameter.');
-    }       
+    
+    if ( (isNaN(width_query) || width_query <= 0) && (isNaN(height_query) || height_query <= 0) ) {
+        response.status(400).send('Invalid width and height parameter.');
+    } else {
+        resize_image(filename,width_query,height_query,response)
+    }
+           
     
 });
 
-const resize_image = async(filename : string,width : number,height : number,response : Response) => {
+const resize_image = async(filename : string,width : number,height : number,response : Response) : Promise<void> => {
     const image_folder_path = "images"
     const image_path_old = path.join(image_folder_path,filename+'.jpg')
     const image_path_new = path.join(image_folder_path,filename+'.png')
-    
-    await fs_promises.access(image_path_old, fs_promises.constants.F_OK).then(() => {
+
+    try {
+            await fs_promises.access(image_path_old, fs_promises.constants.F_OK).then(() => {
                 sharp(image_path_old).resize({ width : width , height : height}).toFile(image_path_new).then(() => {
                         response
                         .status(200)
                         .sendFile(filename+".png",{root : image_folder_path})
                 })
             })
+    } catch (error) {
+        response.status(404).send("Filename not found")
+        // console.log(error)
+    } 
 }
 
 
